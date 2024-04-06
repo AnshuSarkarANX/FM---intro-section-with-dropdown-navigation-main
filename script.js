6d14d80c2a912fbb8c7be70cfd20ed93691004e1
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-04-06 11:44:19 +0530

    changes for largre screen dropdown complete

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const featureDropdown = document.querySelector("#feature-dropdown-wrapper");
 const companyDropdown = document.querySelector("#company-dropdown-wrapper");
+const menubtn = document.querySelector("#burgerButton");
+const closebtn = document.querySelector("#crossButton");
+const sidebar = document.querySelector("#sidenavbar");
+menubtn.addEventListener("click", function () {
+  this.classList.add("is-active");
+  closebtn.classList.add("is-active");
+  sidebar.classList.add("is-active");
+});
+closebtn.addEventListener("click", function () {
+  this.classList.remove("is-active");
+  menubtn.classList.remove("is-active");
+  sidebar.classList.remove("is-active");
+});
 
 featureDropdown.addEventListener("mouseover", function () {
   this.classList.add("is-active");
 });
+
 featureDropdown.addEventListener("mouseout", function () {
   this.classList.remove("is-active");
 });
