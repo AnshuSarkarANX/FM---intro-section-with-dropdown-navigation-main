a080d3b8d99eb91c02cb63c71d37741113e5e9b5
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-04-06 16:19:07 +0530

    challenge complete🥱

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
 const featureDropdown = document.querySelector("#feature-dropdown-wrapper");
 const companyDropdown = document.querySelector("#company-dropdown-wrapper");
+const mobilefeatureDropdown = document.querySelector("#mobile-feature-dropdown-wrapper");
+const mobilecompanyDropdown = document.querySelector("#mobile-company-dropdown-wrapper");
 const menubtn = document.querySelector("#burgerButton");
 const closebtn = document.querySelector("#crossButton");
-const sidebar = document.querySelector("#sidenavbar");
+const sidebar = document.querySelector('.sidebar');
+const blocker = document.querySelector('.blocker');
+ blocker.onclick = toggle;
+ function listToggle(e){
+    e.classList.contains("is-active")
+      ? e.classList.remove("is-active")
+      : e.classList.add("is-active");
+ }
+function toggle() {
+  function show() {
+    sidebar.classList.add("visible");
+    document.body.style.overflow = "hidden";
+  }
+  function hide() {
+    sidebar.classList.remove("visible");
+      closebtn.classList.remove("is-active");
+      menubtn.classList.remove("is-active");
+      mobilefeatureDropdown.classList.remove("is-active");
+      mobilecompanyDropdown.classList.remove("is-active");
+    document.body.style.overflow = "";
+  }
+  sidebar.classList.contains('visible') ? hide() : show();
+}
 menubtn.addEventListener("click", function () {
   this.classList.add("is-active");
   closebtn.classList.add("is-active");
-  sidebar.classList.add("is-active");
+  toggle();
 });
 closebtn.addEventListener("click", function () {
   this.classList.remove("is-active");
   menubtn.classList.remove("is-active");
-  sidebar.classList.remove("is-active");
+  toggle();   
 });
 
 featureDropdown.addEventListener("mouseover", function () {
-  this.classList.add("is-active");
+  listToggle(this);
 });
 
 featureDropdown.addEventListener("mouseout", function () {
-  this.classList.remove("is-active");
+  listToggle(this);
 });
 companyDropdown.addEventListener("mouseover", function () {
-  this.classList.add("is-active");
+  listToggle(this);
 });
 companyDropdown.addEventListener("mouseout", function () {
-  this.classList.remove("is-active");
+  listToggle(this);
 });
-
+mobilefeatureDropdown.addEventListener("click", function () {
+listToggle(this);
+});
+mobilecompanyDropdown.addEventListener("click", function () {
+listToggle(this);
+});
